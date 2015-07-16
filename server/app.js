c099d02ea2c402661917a5ebef27371e88f9a288
Shehzan Devani <shehzandevani@gmail.com>
2015-07-16 09:44:13 -0700

    Listening to heroku port and connecting to heroku db

--- server/app.js
+++ server/app.js
@@ -14,11 +14,22 @@ var config = require('./config/environment');
 
 // Setup database
 var Sequelize = require('sequelize');
-var database = new Sequelize(config.database.dbName, config.database.username, config.database.password, {
-  dialect: 'postgres',
-  host: 'localhost',
-  port: 5432
-});
+var database;
+if (process.env.DATABASE_URL) {
+  // the application is executed on Heroku ... use the postgres database
+  database = new Sequelize(process.env.DATABASE_URL, {
+    dialect:  'postgres',
+    protocol: 'postgres',
+    logging:  true //false
+  })
+} else {
+  // the application is executed on the local machine ... use mysql
+  database = new Sequelize(config.database.dbName, config.database.username, config.database.password, {
+    dialect: 'postgres',
+    host: 'localhost',
+    port: 5432
+  });
+}
 
 // Setup server
 var app = express();
@@ -27,8 +38,9 @@ require('./config/express')(app);
 var server = require('http').createServer(app);
 
 // Start server
-server.listen(config.port, config.ip, function () {
-  console.log('Express server listening on %d, in %s mode', config.port, app.get('env'));
+var port = process.env.PORT;
+server.listen(port, config.ip, function () {
+  console.log('Express server listening on %d, in %s mode', port, app.get('env'));
 });
 
 // Expose app
--- server/config/environment/production.js
+++ server/config/environment/production.js
@@ -10,7 +10,7 @@ module.exports = {
   // Server IP
   ip:       process.env.OPENSHIFT_NODEJS_IP ||
             process.env.IP ||
-            undefined,
+            "0.0.0.0",
 
   // Server port
   port:     process.env.OPENSHIFT_NODEJS_PORT ||
